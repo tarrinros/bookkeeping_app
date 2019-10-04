0beea6930ce78d8d9425ccb68fbee988106f1d5d
tarrinros <tarindis@gmail.com>
2019-10-04 21:09:22 +0300

    Add vue filter for date and time format

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import dateFilter from '@/filters/date.filter'
 import store from './store'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
 Vue.config.productionTip = false
+Vue.filter('date', dateFilter)
 
 new Vue({
   router,
