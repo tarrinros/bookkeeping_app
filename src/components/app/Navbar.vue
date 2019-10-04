dbafa6b67ad09fceb6a0a6038cc5f0e7659d18e4
tarrinros <tarindis@gmail.com>
2019-10-04 14:06:37 +0300

    Move Navbar and Sidebar to components

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -11,9 +11,9 @@
       <ul class="right hide-on-small-and-down">
         <li>
           <a
-              class="dropdown-trigger black-text"
-              href="#"
-              data-target="dropdown"
+            class="dropdown-trigger black-text"
+            href="#"
+            data-target="dropdown"
           >
             USER NAME
             <i class="material-icons right">arrow_drop_down</i>
