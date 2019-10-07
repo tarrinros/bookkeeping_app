a7ac3310a42b67130c588f4630f34f2e89c2eb6d
tarrinros <tarindis@gmail.com>
2019-10-07 19:11:56 +0300

    Add logout message

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -56,6 +56,7 @@
 
 <script>
 import { email, required, minLength } from 'vuelidate/lib/validators'
+import messages from '../utils/messages'
 
 export default {
   name: 'login',
@@ -68,7 +69,9 @@ export default {
     password: { required, minLength: minLength(6) }
   },
   mounted() {
-    this.$error('Test')
+    if (messages[this.$route.query.message]) {
+      this.$message(messages[this.$route.query.message])
+    }
   },
   methods: {
     submitHandler () {
