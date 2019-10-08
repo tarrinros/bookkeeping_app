234a1cbfbf73ce7d1c213fca8867a96e0e94a43d
tarrinros <tarindis@gmail.com>
2019-10-08 15:06:46 +0300

    Add register method for auth module

--- src/store/auth.js
+++ src/store/auth.js
@@ -7,6 +7,25 @@ export default {
       } catch (e) {
         throw e
       }
+    },
+    async register ({ dispatch }, { email, password, name }) {
+      try {
+        await firebase.auth().createUserWithEmailAndPassword(email, password)
+        const uid = dispatch('getUserId')
+        await firebase.database().ref(`/users/${uid}/info`).set({
+          bill: 1000,
+          name: name
+        })
+      } catch (e) {
+        throw e
+      }
+    },
+    getUserId () {
+      const user = firebase.auth().currentUser
+      return user ? user.uid : null
+    },
+    async logout () {
+      await firebase.auth().signOut()
     }
   }
 }
