62b7669287925c02ccfac0f1aebd800fdf33c969
tarrinros <tarindis@gmail.com>
2019-10-10 16:19:57 +0300

    Add submitHandler method for CategoryCreate component

--- src/components/CategoryCreate.vue
+++ src/components/CategoryCreate.vue
@@ -5,23 +5,31 @@
         <h4>Создать</h4>
       </div>
 
-      <form>
+      <form @submit.prevent="submitHandler">
         <div class="input-field">
           <input
               id="name"
               type="text"
+              v-model="title"
+              :class="{invalid: $v.title.dirty && $v.title.required}"
           >
           <label for="name">Название</label>
-          <span class="helper-text invalid">Введите название</span>
+          <span 
+            v-if="$v.title.dirty && $v.title.required"
+            class="helper-text invalid">Введите название категории</span>
         </div>
 
         <div class="input-field">
           <input
               id="limit"
               type="number"
+              v-model="limit"
+              :class="{invalid: $v.limit.dirty && $v.limit.minValue}"
           >
           <label for="limit">Лимит</label>
-          <span class="helper-text invalid">Минимальная величина</span>
+          <span 
+            v-if="$v.limit.dirty && $v.limit.minValue"
+            class="helper-text invalid">Вы ввели неверную минимальную величину. Должно быть не меньше {{$v.limit.$params.minValue.min}}</span>
         </div>
 
         <button class="btn waves-effect waves-light" type="submit">
@@ -34,7 +42,28 @@
 </template>
 
 <script>
+import {requred, minValue} from 'vuelidate/lib/validators'
+import { METHODS } from 'http'
 export default {
-  
+  data: () => ({
+    title: '',
+    limit: 1
+  }),
+  validations: {
+    title: {requred},
+    limit: {minValue: minValue(1)}
+  },
+  methods: {
+    submitHandler () {
+        if (this.$v.$invalid) {
+        this.$v.$touch()
+        return
+      }
+      const formData = {
+        title: this.title,
+        limit: this.limit
+      }
+    }
+  }
 }
 </script>
