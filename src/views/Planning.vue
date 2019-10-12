1e2b0e13a8137ea5cff683ba1c38ecd768272544
tarrinros <tarindis@gmail.com>
2019-10-12 21:56:05 +0300

    Add name, data object and loader to the Planning component

--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <Loader v-if="loading" />
+  <div v-else>
     <div class="page-title">
       <h3>Планирование</h3>
       <h4>12 212</h4>
@@ -21,3 +22,17 @@
     </section>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'planning',
+  data: () => ({
+    loading: true,
+    categories: []
+  }),
+  async mounted () {
+  this.categories = await this.$store.dispatch('fetchCategories')
+  this.loading = false
+  },
+}
+</script>
