20e1e62b67f13532730731828287a355592fc302
tarrinros <tarindis@gmail.com>
2019-10-13 18:23:38 +0300

    Add currencyFilter for progressbar tooltips

--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -15,7 +15,7 @@
           <strong>{{cat.title}}:</strong>
           {{cat.spend | currency}} из {{cat.limit | currency}}
         </p>
-        <div class="progress" v-tooltip="'TESTTEST'">
+        <div class="progress" v-tooltip="cat.tooltip">
           <div
               class="determinate"
               :class="[cat.progressColor]"
@@ -29,6 +29,8 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import currencyFilter from '@/filters/currency.filter'
+
 export default {
   name: 'planning',
   data: () => ({
@@ -59,11 +61,14 @@ export default {
           ? 'yellow'
           : 'red'
 
+      const tooltipValue = cat.limit - spend
+      const tooltip = `${tooltipValue < 0 ? 'Превышение на' : 'Осталось'} ${currencyFilter(Math.abs(tooltipValue))}`
       return {
         ...cat,
         progressPercent,
         progressColor,
-        spend
+        spend,
+        tooltip
       }
     })
 
