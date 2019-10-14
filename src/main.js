96fb575404bf0073776b738359a12e4c35951abb
tarrinros <tarindis@gmail.com>
2019-10-14 20:31:43 +0300

    Add pagination plugin

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
+import Paginate from 'vuejs-paginate'
 import App from './App.vue'
 import router from './router'
 import dateFilter from './filters/date.filter'
@@ -26,6 +27,7 @@ Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
 
 Vue.component('Loader', Loader)
+Vue.component('paginate', Paginate)
 
 firebase.initializeApp({
   apiKey: 'AIzaSyBh6KzX9woFFho-LymTgX9mPymz9EKVgyM',
